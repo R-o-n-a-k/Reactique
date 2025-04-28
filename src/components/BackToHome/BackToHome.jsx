1273569ae790189c5afa6434c7f6b9bfd8e73c83
Ronak <rroonnaakk7@gmail.com>
2025-04-28 17:28:23 +0530

    feat: adding todo project

--- src/components/BackToHome/BackToHome.jsx
+++ src/components/BackToHome/BackToHome.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./BackToHome.css";
 
-const BackToHome = () => {
+const BackToHome = ({ bgColor = "#9B5DE5", bottom = "10%" }) => {
   return (
     <>
       <Link to="/">
-        <i className="fa-solid fa-house back-to-home" />
+        <i
+          className="fa-solid fa-house back-to-home"
+          style={{ backgroundColor: bgColor, bottom: bottom }}
+        />
       </Link>
     </>
   );
