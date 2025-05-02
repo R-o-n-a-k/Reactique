fedb8a5806902aced8d6fcd695a2628deb8d8c58
Ronak <rroonnaakk7@gmail.com>
2025-05-02 14:55:40 +0530

    feat: adding contact form and toaster

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 import AnimateOnScroll from "./components/AnimateOnScroll";
+import ToastAlert from "./components/ToastAlert";
 
 function App() {
   return (
     <>
+      <ToastAlert />
       <Router>
         <AppRoutes />
       </Router>
